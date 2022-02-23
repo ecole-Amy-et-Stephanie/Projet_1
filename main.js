8ff7ddda6cdaefb1cdc5840c9793ab194f596caf
Amy Bienvenu <amy.bienvenu@outlook.com>
2022-02-23 16:40:09 -0500

    patch - /results devient /movies

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ const PORT = 3000;
 let app = new express();
 
 app.set("view engine","ejs");
-app.set("views", "views");
+// app.set("views", "views");
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on PORT ${PORT}.`);
 });
 
-app.use(express.static(publicPath, {extensions: ['css','js','jpg']}));
+app.use(express.static(publicPath, {extensions: ['html','htm','css','js','jpg','gif','png']}));
 
 /* requests */
 app.get('/search', aController.getIndex);
 
-app.get('/results', (request, response) => {
+app.get('/movies', (request, response) => {
     let keywords = request.query.search;
     let search = "https://api.themoviedb.org/3/search/movie?api_key=036ea19c89b3277d8f1857ad51e3cb4c&query="+keywords;
     axios.get(search)
